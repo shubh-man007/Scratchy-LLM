d569c495e0b545357c222a7992975e85227c00ff
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2025-08-06 16:53:09 +0530

    Add files via upload

---
+++ agents/ReAct/crawler/readability_extractor.py
@@ -0,0 +1,14 @@
+# From: https://github.com/bytedance/deer-flow/blob/main/src/crawler/readability_extractor.py
+
+from readabilipy import simple_json_from_html_string
+
+from .article import Article
+
+
+class ReadabilityExtractor:
+    def extract_article(self, html: str) -> Article:
+        article = simple_json_from_html_string(html, use_readability=True)
+        return Article(
+            title=article.get("title"),
+            html_content=article.get("content"),
+        )
