ece1d021a6d25595cf71a6cbc30975e0cd7b3f36
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2025-08-15 22:52:50 +0530

    Update agent.py

--- agents/T5-Agent/manager/agent.py
+++ agents/T5-Agent/manager/agent.py
@@ -1,10 +1,10 @@
 from google.adk.agents import Agent
 
-from .tools.model import generate_sql_query
+from .tools.model import generate_sql_query, review_database_schema, list_tables, preview_table_data
 
 root_agent = Agent(
     name = "manager",
     model = "gemini-2.0-flash",
     description = "An agent that takes performs Text to SQL conversions",
-    tools = [generate_sql_query],
+    tools = [generate_sql_query, review_database_schema, list_tables, preview_table_data],
 )
