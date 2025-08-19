f17d73bc97dc86ec0d08f59bbf2abdd0132c45a2
Shubhodeep De <163862265+shubh-man007@users.noreply.github.com>
2025-08-19 15:47:51 +0530

    Update crawler.py

--- agents/ReAct/tools/crawler.py
+++ agents/ReAct/tools/crawler.py
@@ -1,6 +1,7 @@
 # Reference: https://github.com/bytedance/deer-flow/blob/main/src/tools/crawl.py
 from ..crawler.crawler import Crawler
 from .tools import Tool
+from typing import Union, Dict
 import logging
 
 logger = logging.getLogger(__name__)
@@ -19,3 +20,19 @@ class CrawlerTool(Tool):
             error_msg = f"Failed to crawl. Error: {repr(e)}"
             logger.error(error_msg)
             return error_msg
+    
+    # According to our tool definition
+    def run(self, input: Union[str, Dict[str, str]]) -> Dict[str, str]:
+        if isinstance(input, dict):
+            url = input.get("url", "")
+        else:
+            url = input
+        
+        try:
+            crawler = Crawler()
+            article = crawler.crawl(url)
+            return {"url": url, "crawled_content": article.to_markdown()[:1000]}
+        except BaseException as e:
+            error_msg = f"Failed to crawl. Error: {repr(e)}"
+            logger.error(error_msg)
+            return {"error": error_msg, "url": url}
